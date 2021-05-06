387ece2d2e3cc6f8f2e0f9b99febdbf166a7ce2b
Tolga Ünlü <9641840+tolgadevsec@users.noreply.github.com>
2021-05-06 20:48:54 +0200

    Update monkeypatch.js
    Added a more generic detection approach based on code presented @ https://gomakethings.com/how-saferinnerhtml-works/

--- 2021/Abertay-EH-Society/monkeypatch.js
+++ 2021/Abertay-EH-Society/monkeypatch.js
@@ -1,9 +1,30 @@
 // Source: https://stackoverflow.com/a/39561071
-var originalSet = Object.getOwnPropertyDescriptor(Element.prototype, "innerHTML").set;
+const originalSet = Object.getOwnPropertyDescriptor(Element.prototype, "innerHTML").set;
+
+const inlineHandlers = [
+    "onerror"
+];
+
+const domParser = new DOMParser();
   
 Object.defineProperty(Element.prototype, "innerHTML", {
     set: function (value) {
-        console.log(value);
+        //console.log(value);
+
+        // Source: https://gomakethings.com/how-saferinnerhtml-works/
+        let parsedElement = domParser.parseFromString(value, "text/html");
+        if(parsedElement.body && parsedElement.body.hasChildNodes){
+            let detectedInlineHandlers = [];
+            Array.from(parsedElement.body.childNodes).forEach((childNode) => {
+                if(childNode.hasAttributes()){
+                    detectedInlineHandlers = inlineHandlers.filter((inlineHanlder) => childNode.attributes.getNamedItem(inlineHanlder) !== null);
+                }
+            });
+
+            if(detectedInlineHandlers.length > 0){
+                console.log("[INNERHTML] Detected inline event handlers '" + detectedInlineHandlers.toString() + "' in " + value);
+            }
+        }
         
         return originalSet.call(this, value);
     }
